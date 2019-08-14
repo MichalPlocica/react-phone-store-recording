ea9b1620d617a8cee91102129c7b3b23280f5e68
MichalPlocica <michalplocica@gmail.com>
2019-08-14 16:45:16 +0200

    3neu +koment

--- src/data.js
+++ src/data.js
@@ -103,7 +103,7 @@ export const storeProducts = [
   {id: 9,
     title: "Herbadent Zahnpasta",
     img: "img/product-9.png",
-    price: 2,
+    price: 10,
     company: "apple",
     info:
       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
@@ -114,7 +114,7 @@ export const storeProducts = [
   {id: 10,
     title: "Opalescence 12x",
     img: "img/product-10.png",
-    price: 2,
+    price: 240,
     company: "apple",
     info:
       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
@@ -125,7 +125,7 @@ export const storeProducts = [
   {id: 11,
     title: "Opalescence 1x",
     img: "img/product-11.png",
-    price: 2,
+    price: 26,
     company: "apple",
     info:
       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
