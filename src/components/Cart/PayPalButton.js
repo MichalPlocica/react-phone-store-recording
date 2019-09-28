c90b2dada465b11d4df401c3ece96b41dc905826
MichalPlocica <michalplocica@gmail.com>
2019-09-28 17:17:47 +0200

    Update PayPalButton.js

--- src/components/Cart/PayPalButton.js
+++ src/components/Cart/PayPalButton.js
@@ -24,7 +24,7 @@ export default class MyApp extends React.Component {
       // => sometimes it may take about 0.5 second for everything to get set, or for the button to appear
     };
 
-    let env = "production"; // you can set here to "sandbox" or 'production' for production
+    let env = "sandbox"; // you can set here to "sandbox" or 'production' for production
     let currency = "CHF"; // or you can set this value from your props or state
     //let total = 1;
     // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
